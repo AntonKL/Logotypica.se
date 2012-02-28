db43f0efb7c9a9302765ebc39782355e0fa018d0
Anton Kjörck Lindén <anton@noviumdesign.se>
2012-02-28 16:04:39 +0100

    Changed typo, added new imgs, added favicon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<link rel="icon" href="favicon.png" />
 		<title>Håkan Schallinger</title>
 
 		<!-- LESS converted to css and minified via build script -->
@@ -16,17 +17,18 @@
 		<div id="wrapper">
 			<div class="upper clearfix">
 				<div class="left">
-					<nav>Logotypica Design</nav>
-					<div class="desc">Håkan Schallinger är senior designer och utvecklar designkonceptet och grafiska profiler. Håkan Schallinger har arbetat med varumärken som Felix, Cancerfonden, ProViva och Gant och drev under många är designbyrån Amore i Stockholm.</div>
+					<nav>Logotypica</nav>
+					<div class="desc">Håkan Schallinger är en stockholmsbaserad designer som arbetar med förpackningsdesign och grafiska identiteter. Han har utvecklat designkoncept för varumärken som ProViva, Felix och Skånemejerier.</div>
 					<div class="contact">
-						<p>Håkan Schallinger</p>
-						<p>Senior designer</p>
-						<p>hakan@logotypica.se</p>
+						<p>Tjänster:</p>
+						<p>Förpackningsdesign</p>
+						<p>Grafisk identitet</p>
+						<p>Logotyp & symboler</p>
+						<p>Illustration</p>
 						<section>
-							<p>Logotypica &reg;</p>
-							<p>Granevägen 4</p>
-							<p>182 55 Stockholm</p>
-							<p>Org. nr: 556735-4716</p>
+							<p>Kontakt:</p>
+							<p>hakan@logotypica.se</p>
+							<p>+46 (0) 70-791 88 97</p>
 						</section>
 					</div>
 				</div>
@@ -36,7 +38,11 @@
 						<div class="ribbon"></div>
 						<div id="slider" class="nivoSlider">
 							<img src="images/projects/leafs.png" alt="" />
+							<img src="images/projects/cider.jpg" alt="" />
 							<img src="images/projects/rabbit.png" alt="" />
+							<img src="images/projects/lll.jpg" alt="" />
+							<img src="images/projects/etac.jpg" alt="" />
+							<img src="images/projects/skanemejerier.jpg" alt="" />
 						</div>
 					</div>
 				</div>
@@ -51,8 +57,8 @@
 		<script type="text/javascript">
 			$(window).load(function() {
 				$('#slider').nivoSlider({
-					effect: 'fade,sliceDown', // Specify sets like: 'fold,fade,sliceDown'
-					pauseTime: 5000, // How long each slide will show
+					effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+					pauseTime: 3000, // How long each slide will show
 					pauseOnHover: false, // Stop animation while hovering
 				});
 			});
